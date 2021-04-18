080af0492994f362c0361c0b1a19e8b2bf15f2de
Rob174 <robin.128.456@gmail.com>
2021-04-18 14:26:10 +0100

    Ajout de layers

--- src/tools/update_layers_json.py
+++ src/tools/update_layers_json.py
@@ -1,4 +1,6 @@
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from tensorflow.python.keras.layers import BatchNormalization, Dropout, Activation, GlobalAveragePooling2D, Add, \
+    SeparableConv2D
 
 from src.tools.SaveWriter import SaveWriter
 
@@ -7,10 +9,11 @@ def update_layer(layer):
     args = list(
         filter(lambda x: x != "self" and x != "kwargs" and x != "args", layer.__init__.__code__.co_varnames))
     with SaveWriter("../analyser/layers.json") as dico:
+        dico[layer.__name__] = {"attributes":{},"format":{}}
         dico[layer.__name__]["attributes"] = {arg:"same" for arg in args}
 
 
 if __name__ == "__main__":
-    for layer in [Conv2D, MaxPooling2D, Flatten, Dense]:
+    for layer in [BatchNormalization,Dropout,Activation,SeparableConv2D,Add,GlobalAveragePooling2D]:
         print("Layer")
         update_layer(layer)
